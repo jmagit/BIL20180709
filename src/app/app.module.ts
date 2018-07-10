fae017235fe599c789cea2f90754b4f1436381a0
formacion <formacion@ANSFORM26.indra.es>
2018-07-10 11:47:20 +0200

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MyCoreModule } from '../my-core';
+import { ComunesModule } from './comunes/comunes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    MyCoreModule, ComunesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/my-core/my-core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class MyCoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: MyCoreModule) {
+    if (parentModule) {
+      const msg = `ModuleName has already been loaded.
+        Import ModuleName once, only, in the root AppModule.`;
+      throw new Error(msg);
+    }
+  }
+}
