b26e676b198b66c136b348cd34b857ffc422ced3
formacion <formacion@ANSFORM26.indra.es>
2018-07-12 11:42:21 +0200

    dinamico

--- src/app/dinamico/dinamico.component.ts
+++ src/app/dinamico/dinamico.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { DemosComponent } from '../demos/demos.component';
+import { CalculadoraComponent } from '../calculadora/calculadora.component';
 
 @Component({
   selector: 'app-dinamico',
   templateUrl: './dinamico.component.html',
-  styleUrls: ['./dinamico.component.css']
+  styleUrls: ['./dinamico.component.css'],
+  entryComponents: [ HomeComponent, DemosComponent, CalculadoraComponent, ],
 })
 export class DinamicoComponent implements OnInit {
+  menu = [
+    { texto: 'Inicio', componete: HomeComponent},
+    { texto: 'Demos', componete: DemosComponent},
+    { texto: 'Calculadora', componete: CalculadoraComponent},
+  ];
+  seleccionado = this.menu[0].componete;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  seleccionar(indice: number) {
+    this.seleccionado = this.menu[indice].componete;
+  }
 }
