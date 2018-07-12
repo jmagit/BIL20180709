9c8eca73a561cf099ee9a90a4452f5fa17357a29
formacion <formacion@ANSFORM26.indra.es>
2018-07-12 13:17:55 +0200

    views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { DemosComponent } from './demos/demos.component';
 import { NotificationComponent } from './notification/notification.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { DinamicoComponent } from './dinamico/dinamico.component';
-import { PersonasComponent } from './personas/personas.component';
+import { PERSONAS_COMPONENT } from './personas/personas.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { PersonasComponent } from './personas/personas.component';
     NotificationComponent,
     CalculadoraComponent,
     DinamicoComponent,
-    PersonasComponent
+    PERSONAS_COMPONENT,
   ],
   imports: [
     BrowserModule, FormsModule,
